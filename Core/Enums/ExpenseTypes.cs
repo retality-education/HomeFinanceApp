f5e875ac476c06dac0ab2a9f01c3940a87d24fc6
Ольга <@LAPTOP-LBP3SJV4>
2025-05-05 08:50:52 +0300

    Добавьте файлы проекта.

---
+++ Core/Enums/ExpenseTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeFinanceApp.Core.Enums
+{
+    internal enum ExpenseTypes
+    {
+        Food = 0,
+        Utilities = 1,
+        Clothing = 2,
+        Entertainment = 3,
+        Education = 4,
+        Cosmetics = 5,
+        Hobby = 6
+    }
+}
---
+++ Core/Interfaces/IObserver.cs
@@ -0,0 +1,14 @@
+﻿using HomeFinanceApp.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeFinanceApp.Core.Interfaces
+{
+    internal interface IObserver
+    {
+        void OnFamilyEvent(FamilyEvents eventType, int memberId, decimal money = 0);
+    }
+}
